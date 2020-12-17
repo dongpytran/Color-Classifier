cdcd893af5a26e508cfca2339f61fab4b9b17fd0
Dong Py Tran <67514451+dongpytran@users.noreply.github.com>
2020-12-17 21:25:43 +0700

    Update training.js

--- Color/training.js
+++ Color/training.js
@@ -3,7 +3,6 @@ let model;
 let xs, ys;
 let rSlider, gSlider, bSlider;
 let labelP;
-let lossP;
 let labelL;
 //list nhan mau
 let labelList = [
@@ -23,26 +22,51 @@ function preload() {
   data = loadJSON('colorData.json');
 }
 
-//setup canvas, sliderbar
+
 function setup() {
-  // Crude interface
+  //Interface
   labelL = createP('Màu bạn chọn thuộc miền :');
   labelP = createP('---');
-  lossP = createP('loss');
-  labelL = createP('R:');
+  labelP.style('font-size', '25px');
+  labelR = createInput('R: ');
+  labelR.style('width', '20px');
+  labelR.style('border', 'none');
+  labelR.style('font-size', '20px');
+  labelR.style('color', '#000');
+  labelR.style('background', 'transparent');
+  labelR.attribute('disabled', '');
   rSlider = createSlider(0, 255, 255);
   rSlider.style('width', '150px');
   inputR2 = createInput();
   inputR2.id('rValue');
+  inputR2.attribute('disabled', '');
 
+
+  labelG = createInput('G: ');
+  labelG.style('width', '20px');
+  labelG.style('border', 'none');
+  labelG.style('font-size', '20px');
+  labelG.style('color', '#000');
+  labelG.style('background', 'transparent');
+  labelG.attribute('disabled', '');
   gSlider = createSlider(0, 255, 255);
   inputG2 = createInput();
   inputG2.id('gValue');
+  inputG2.attribute('disabled', '');
   gSlider.style('width', '150px');
 
+
+  labelB = createInput('B: ');
+  labelB.style('width', '20px');
+  labelB.style('border', 'none');
+  labelB.style('font-size', '20px');
+  labelB.style('color', '#000');
+  labelB.style('background', 'transparent');
+  labelB.attribute('disabled', '');
   bSlider = createSlider(0, 255, 255);
   inputB2 = createInput();
   inputB2.id('bValue');
+  inputB2.attribute('disabled', '');
   bSlider.style('width', '150px');
 
   let colors = []; //khoi tao 1 mang luu tru cac mau
@@ -50,17 +74,18 @@ function setup() {
   for (let record of data.entries) {//doc moi dong trong file data json
     let col = [record.r / 255, record.g / 255, record.b / 255]; //dua du lieu ve khoang 0-1
     colors.push(col);//add color vao mang colors
-    labels.push(labelList.indexOf(record.label));//add nhan cua mau do(indexOf) vao list mau
+    labels.push(labelList.indexOf(record.label));//lay vi tri nhan cua mau do trong list nhan, add vao mang
   }
-
-  xs = tf.tensor2d(colors);
-  let labelsTensor = tf.tensor1d(labels, 'int32');
-
+  console.log(colors);
+  console.log(labels);
+  xs = tf.tensor2d(colors); // convert mang colors sang kieu du lieu tensor
+  let labelsTensor = tf.tensor1d(labels, 'int32'); //convert mang nhan sang kieu du lieu tensor
+  console.log(xs.shape);
   ys = tf.oneHot(labelsTensor, 9).cast('float32');
-  labelsTensor.dispose();
-
+  labelsTensor.dispose(); // loai bo tensor khoi bo nho
+  console.log(ys.shape);
   //khoi tao model
-  model = tf.sequential();
+  model = tf.sequential(); //mo hinh tuan tu: dau ra mot lop la dau vao cua lop tiep theo
   //khoi tao lop hidden
   const hidden = tf.layers.dense({
     units: 16, //4, 16 , 32 tuy
@@ -75,13 +100,13 @@ function setup() {
   model.add(hidden); //add lop hidden vao model vua tao
   model.add(output); // add lop output vao model vua tao
 
-  const LEARNING_RATE = 0.25; // toc do hoc
+  const LEARNING_RATE = 0.25; // Ty le hoc
   const optimizer = tf.train.sgd(LEARNING_RATE); // train theo rate
-
+  //cau hinh model, cbi train
   model.compile({
-    optimizer: optimizer,
-    loss: 'categoricalCrossentropy',
-    metrics: ['accuracy']
+    optimizer: optimizer, // trinh toi uu hoa
+    loss: 'categoricalCrossentropy', //Giá trị mat mat sẽ được mô hình tối thiểu hóa,sau đó sẽ là tổng của tất cả các tổn thất riêng lẻ.
+    metrics: ['accuracy'] //chỉ số được đánh giá bởi mô hình trong quá trình train va test
   });
 
   train();
@@ -99,9 +124,8 @@ async function train() {
       onTrainEnd: () => {yoBro()}, // ket thuc train
       onEpochEnd: (nums, logs) =>{ // voi moi lan training
         console.log('Epoch: '+ nums); // in ra lan train thu ?
-        console.log('Loss:'+ logs.loss) // in ra mat mat
-        lossP.html('Độ chính xác :' + (100-(logs.loss.toFixed(2))*10) + ' %');
-        var per = ((parseInt(nums)+2) * 2)
+        console.log('Loss:'+ logs.loss);// in ra mat mat
+        var per = ((parseInt(nums)+2) * 2);
         percent.innerHTML = per + ' %';
       }
     }
@@ -124,12 +148,14 @@ function draw() {
     document.body.style.backgroundColor = '#fff';
   }else{
     document.body.style.backgroundColor = 'rgb(' + r + ',' + g + ',' + b + ')';
-    tf.tidy(() => {
+    tf.tidy(() => { //Sử dụng phương pháp này giúp tránh rò rỉ bộ nhớ
       const input = tf.tensor2d([[r/255, g/255, b/255]]); //lay input tu silderbar
-      let results = model.predict(input); //du doan ket qua
-      let argMax = results.argMax(1);
-      let index = argMax.dataSync()[0];
+      let results = model.predict(input); //Thực hiện suy luận cho các tensor đầu vào.
+      let argMax = results.argMax(1); //Trả về các chỉ số của giá trị lớn nhất (1) dọc theo nhan.
+      let index = argMax.dataSync()[0];//đồng bộ các giá trị từ tf.Tensor
+      //vd: 0 0 0 1 0 => index = 3
       let label = labelList[index];
+      //lay label do
       labelP.html(label);
   });
   }
